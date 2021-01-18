7a5a6756865114e828abb2e000320004a80b65dd
Matthew Weaber <matthewweaber89@gmail.com>
2021-01-18 16:56:03 -0500

    css in index.css

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -1,9 +1,19 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+
+const Header = ({ title }) => {
     return (
         <header>
-            <h1>Task Tracker</h1>
+            <h1>{title}</h1>
         </header>
     )
+}
+
+Header.defaultProp = {
+    title: 'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
