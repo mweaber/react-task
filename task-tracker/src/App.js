8c80de56a7e7b1a03fc0c194f3a808e7e560c1d9
Matthew Weaber <matthewweaber89@gmail.com>
2021-01-28 18:55:08 -0500

    add task form

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -26,6 +26,11 @@ const App = () => {
     }
   ])
 
+  // Add Task
+  const addTask = (task) => {
+    console.log(task);
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -39,8 +44,8 @@ const App = () => {
   return (
     <div className="container">
       <Header title='Task Tracker' />
-      <AddTask />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}  /> : 'No Tasks To Show'}
     </div>
   );
 }
